3e93c4ea41abecc8a6de47e2a6cb8464767a720b
Pablo Gonzalez <alu0100887037@ull.edu.es>
2018-10-14 13:29:19 +0100

    C3: Imprime sólo una línea

--- ej01.rb
+++ ej01.rb
@@ -1,9 +1,10 @@
-puts "Hola mundo!"
-puts "Hello es hola en inglés."
-puts "Estas frases no tienen mucho sentido."
-puts "Pero es divertido."
+# puts "Hola mundo!"
+# puts "Hello es hola en inglés."
+# puts "Estas frases no tienen mucho sentido."
+# puts "Pero es divertido."
 puts "Y se logra que en la terminal se muestre algo."
-puts "Yo he escrito más fases que tú."
-puts '¡No toques eso!'
-puts "Esta es la nueva línea que se va a imprimir"
+# puts "Yo he escrito más fases que tú."
+# puts '¡No toques eso!'
+# puts "Esta es la nueva línea que se va a imprimir"
+
 
